2ad9a4e1012ce2b5d1943d824c5b7b0acc5119d4
wahidul <wahidulhassan60@gmail.com>
2025-04-29 13:37:41 +0600

    topbar ok

--- src/Layout/TopBar.jsx
+++ src/Layout/TopBar.jsx
@@ -2,23 +2,38 @@ import React from 'react'
 import Flex from '../Component/Flex'
 import Container from '../Component/Container'
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import { IoMdSearch } from "react-icons/io";
+import { IoMdContact } from "react-icons/io";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { IoCartOutline } from "react-icons/io5";
+
 
 
 const TopBar = () => {
   return (
-   <section>
+   <section className='bg-[#e4e4e4] '>
     <Container>
-        <Flex>
-            <div className='w-4/12 bg-red-500'>
+        <Flex className='justify-center items-center '>
+            <div className='w-4/16'>
                 <Flex className='gap-3'>
                     <HiOutlineMenuAlt4 />
                     <h2 className='font-DM font-normal text-[14px] text-[#262626] '>Shop by Category</h2>
                 </Flex> 
             </div>
-            <div className='w-7/12 bg-black'>
-                <input type="text" className='w-full bg-white font-DM font-normal text-[14px] px-6 py-6' placeholder='Search Products' />
+            <div className='w-8/16 relative'>
+                <input type="text" className='w-full bg-white font-DM font-normal text-[14px] px-4 py-4 mx-6 my-6 rounded-' placeholder='Search Products' />
+                <div className='absolute top-1/2 right-0 -translate-1/2'><IoMdSearch /></div>
             </div>
-            <div className='w-1/12 h-5 bg-green-200'></div>
+            <div className='w-4/16 '>
+                <Flex className=' justify-end gap-5'>
+                    <Flex>
+                        <IoMdContact className='text-black' />
+                        <IoMdArrowDropdown className='text-black' />  
+                    </Flex>
+                    <IoCartOutline />   
+                </Flex>
+                           
+            </div>
         </Flex>
     </Container>
     
